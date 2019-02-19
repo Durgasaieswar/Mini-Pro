7aa9566011aa18004c07e783473d30c822eeab7f
Durgasaieswar <durgasaieswar@gmail.com>
2019-02-19 20:31:56 +0530

    My project commit

---
+++ parties.js
@@ -0,0 +1,42 @@
+var inc=document.querySelector(".btn-large");
+
+inc.addEventListener("mouseover",function(){
+	this.style.color="brown";	
+});
+
+inc.addEventListener("mouseout",function(){
+	this.style.color="black";	
+});
+
+var bjp=document.querySelector(".btn-medium");
+
+bjp.addEventListener("mouseover",function(){
+	this.style.color="brown";	
+});
+
+bjp.addEventListener("mouseout",function(){
+	this.style.color="black";	
+});
+
+var jdp=document.querySelector(".btn-tiny");
+
+jdp.addEventListener("mouseover",function(){
+	this.style.color="brown";	
+});
+
+jdp.addEventListener("mouseout",function(){
+	this.style.color="black";	
+});
+
+var kjp=document.querySelector(".btn-small");
+
+kjp.addEventListener("mouseover",function(){
+	this.style.color="brown";	
+});
+
+kjp.addEventListener("mouseout",function(){
+	this.style.color="black";	
+});
+
+
+
